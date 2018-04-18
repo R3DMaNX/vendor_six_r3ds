c66d49d8edf5bc5e39dc6d55c171f92b79729faf
R3Ds <fpoolboy@gmail.com>
2018-04-17 20:14:47 -0400

    OCD: Rework bootanimation script

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,30 +1,27 @@
-# Boot Animation
-ifeq ($(filter taimen,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/nephilim/prebuilt/common/media/1440x2880.zip:system/media/bootanimation.zip
-endif
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-ifeq ($(filter shamu angler,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/nephilim/prebuilt/common/media/1440x2560.zip:system/media/bootanimation.zip
+# Add Stock Lollipop bootanimation based on device
+ifneq ($(filter angler,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nephilim/prebuilt/bootanimation/angler.zip:system/media/bootanimation.zip
 endif
-
-ifeq ($(filter dumpling,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/nephilim/prebuilt/common/media/1080x2160.zip:system/media/bootanimation.zip
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nephilim/prebuilt/bootanimation/shamu.zip:system/media/bootanimation.zip
 endif
-
-ifeq ($(filter hammerhead cheeseburger oneplus3 potter,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/nephilim/prebuilt/common/media/1080x1920.zip:system/media/bootanimation.zip
-endif
-
-ifeq ($(filter tenderloin,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/nephilim/prebuilt/common/media/768x1024.zip:system/media/bootanimation.zip
-endif
-
-ifeq ($(filter dragon,$(TARGET_PRODUCT)),)
-PRODUCT_COPY_FILES += \
-    vendor/nephilim/prebuilt/common/media/1800x2560.zip:system/media/bootanimation.zip
+ifneq ($(filter walleye,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/nephilim/prebuilt/bootanimation/walleye.zip:system/media/bootanimation.zip
 endif
