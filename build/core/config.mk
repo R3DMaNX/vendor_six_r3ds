Havoc-OS/android_vendor_havoc

vendor: Cleanup

build/core/config.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
+# Rules for MTK targets
+include $(TOPDIR)vendor/havoc/build/core/mtk_target.mk
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/havoc/build/core/qcom_target.mk
+
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/havoc/build/core/system_rro.mk

config/common.mk
@@ -6,28 +6,17 @@ EXCLUDE_SYSTEMUI_TESTS := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
-    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
-    ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
     ro.com.android.dataroaming=false \
-    drm.service.enabled=true \
-    net.tethering.noprovisioning=true \
-    ro.adb.secure=0 \
-    ro.build.selinux=1 \
-    ro.setupwizard.rotation_locked=true \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.opa.eligible_device=true \
-    persist.sys.disable_rescue=true
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-    ro.adb.secure=0 \
-    ro.debuggable=1 \
-    ro.secure=0 \
-    persist.service.adb.enable=1 \
-    persist.sys.usb.config=mtp,adb
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.build.selinux=1 \
+    ro.carrier=unknown
 
 PRODUCT_PROPERTY_OVERRIDES := \
     persist.sys.wfd.nohdcp=1 \
@@ -80,9 +69,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
-# Copy all Havoc-specific init rc files
+# Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/init.local.rc:root/init.havoc.rc
+    vendor/havoc/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+
+# Copy all Havoc-specific init rc files
+$(foreach f,$(wildcard vendor/havoc/prebuilt/common/etc/init/*.rc),\
+	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
@@ -106,7 +99,7 @@ PRODUCT_COPY_FILES += \
 
 # Fix Google dialer
 PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+    vendor/havoc/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
 # Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += \
@@ -116,10 +109,11 @@ PRODUCT_PACKAGES += \
 # Latin IME lib
 ifeq ($(TARGET_ARCH),arm64)
 PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/havoc/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
     vendor/havoc/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 else
 PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/havoc/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 

config/telephony.mk
@@ -1,10 +1,18 @@
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/havoc/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
-
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/havoc/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \

R3DMaNX/vendor_six_r3ds

build/core/config.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
+# Rules for MTK targets
+include $(TOPDIR)vendor/six/build/core/mtk_target.mk
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/six/build/core/qcom_target.mk
+
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/six/build/core/system_rro.mk

config/common.mk
@@ -6,25 +6,22 @@ EXCLUDE_SYSTEMUI_TESTS := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
-    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
-    ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
     ro.com.android.dataroaming=false \
-    ro.adb.secure=0 \
-    ro.build.selinux=1 \
     ro.setupwizard.rotation_locked=true \
-    persist.sys.disable_rescue=true
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
     ro.adb.secure=0 \
     ro.debuggable=1 \
     ro.secure=0 \
     persist.service.adb.enable=1 \
-    persist.sys.usb.config=mtp,adb
+    persist.sys.usb.config=mtp,adb \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.build.selinux=1 \
+    ro.carrier=unknown
 
 PRODUCT_PROPERTY_OVERRIDES := \
     persist.sys.wfd.nohdcp=1 \
@@ -86,9 +83,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/six/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
-# Copy all six-specific init rc files
+# Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
-    vendor/six/prebuilt/common/etc/init.local.rc:root/init.six.rc
+    vendor/six/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+
+# Copy all six-specific init rc files
+$(foreach f,$(wildcard vendor/six/prebuilt/common/etc/init/*.rc),\
+	$(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
@@ -112,7 +113,7 @@ PRODUCT_COPY_FILES += \
 
 # Fix Google dialer
 PRODUCT_COPY_FILES += \
-    vendor/six/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+    vendor/six/prebuilt/common/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
 
 # Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += \
@@ -122,10 +123,11 @@ PRODUCT_PACKAGES += \
 # Latin IME lib
 ifeq ($(TARGET_ARCH),arm64)
 PRODUCT_COPY_FILES += \
-    vendor/six/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/six/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
     vendor/six/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 else
 PRODUCT_COPY_FILES += \
+    vendor/six/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
     vendor/six/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 

config/telephony.mk
@@ -1,10 +1,18 @@
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/six/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
-
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/six/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/six/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/six/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/six/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \